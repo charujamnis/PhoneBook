2a0bae86b6ca95ba5cd79945d6f26e731b12e3f1
charujamnis <charushila.jamnis@gmail.com>
2020-10-03 11:45:27 -0400

    Assignment complete

--- src/main/java/com/dtcc/projects/PhoneBook.java
+++ src/main/java/com/dtcc/projects/PhoneBook.java
@@ -1,4 +1,65 @@
 package com.dtcc.projects;
 
+import java.util.*;
+
 public class PhoneBook {
+    Map<String, List<String>> phoneBookMap;
+
+    public PhoneBook(){
+        this(new LinkedHashMap<String,List<String>>());
+    }
+
+    public PhoneBook(Map<String,List<String>> map){
+        this.phoneBookMap=map;
+    }
+
+    public void add(String name,String phoneNumber){
+        List<String> phoneNumberList=phoneBookMap.get(name);
+       if(phoneNumberList!=null){
+           phoneNumberList.add(phoneNumber);
+       }
+       else{
+           phoneBookMap.put(name,Collections.singletonList(phoneNumber));
+       }
+    }
+
+    public void addAll(String name, String[] pnumbers){
+        if(!phoneBookMap.containsKey(name)) {
+            phoneBookMap.put(name,Arrays.asList(pnumbers));
+        }
+    }
+
+    public void remove(String name){
+        if(phoneBookMap.containsKey(name))
+            phoneBookMap.remove(name);
+    }
+
+    public boolean hasEntry(String name){
+        return phoneBookMap.containsKey(name);
+    }
+
+    public List<String> lookup(String name){
+        List<String> sendList=new ArrayList<>();
+        for(Map.Entry<String,List<String>> e: phoneBookMap.entrySet()){
+            if(e.getKey().equalsIgnoreCase(name)){
+                sendList= e.getValue();
+            }
+        }
+        return sendList;
+    }
+
+    public String reverseLookup(String phoneNumber){
+        String strName="";
+        for(Map.Entry<String,List<String>> e: phoneBookMap.entrySet()) {
+            if(e.getValue().contains(phoneNumber)){
+                strName=e.getKey();
+            }
+        }
+        return strName;
+    }
+
+    public Set<String> getAllContactNames(){
+        return phoneBookMap.keySet();
+    }
+
 }
